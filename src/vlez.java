d8852352dd075a265f8c7cbc009cd40fc562619f
Zmijaa <mircevfilip98@gmail.com>
2023-12-29 09:16:51 +0100

    first commit

---
+++ src/vlez.java
@@ -0,0 +1,23 @@
+
+public class vlez {
+double a, b, c;
+public vlez(double a, double b, double c) {
+this.a = a;
+this.b = b;
+this.c = c;
+}
+public double strana_a() {
+return a;
+}
+public double strana_b() {
+return b;
+
+}
+public double strana_c() {
+return c;
+}
+public boolean rezultat()
+{
+return (a+b>c && a+c>b && b+c>a);
+}
+}
